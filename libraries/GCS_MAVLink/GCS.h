2fb5d19e76111de63d1b5533274273d1b5d282b8
Ömer Efe ÇELİK <heblushabus@aaal.net.tr>
2024-08-07 14:27:55 +0300

    Update GCS.h

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -629,7 +629,7 @@ protected:
 
     void send_timesync();
     // returns the time a timesync message was most likely received:
-    uint64_t timesync_receive_timestamp_ns() const;
+    uint64_t timesync_receive_timestamp_us() const;
     // returns a timestamp suitable for packing into the ts1 field of TIMESYNC:
     uint64_t timesync_timestamp_ns() const;
     void handle_timesync(const mavlink_message_t &msg);
